b3ed34daef8528f74f82a7042d9e75b9de856e73
Melissa <wolfaholic@users.noreply.github.com>
2021-02-15 08:48:41 -0800

    Uploading

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { Route, useHistory } from "react-router-dom";
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie'
 
-export default function App () {
+const App = () => {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
   const [movieList, setMovieList] = useState([]);
 
   useEffect(() => {
     const getMovies = () => {
       axios
-        .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
+        .get('http://localhost:5000/api/movies')
         .then(response => {
-          // Study this response with a breakpoint or log statements
-          // and set the response data as the 'movieList' slice of state
+          setMovieList(response.data);
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -29,8 +30,14 @@ export default function App () {
   return (
     <div>
       <SavedList list={[ /* This is stretch */]} />
-
-      <div>Replace this Div with your Routes</div>
+      <div>
+        <Route exact path='/' component={MovieList} />
+        <Route path="/Movies/:id" component={Movie} />  
+      </div>
     </div>
   );
-}
+};
+
+export default 
+App;
+
--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,5 +1,51 @@
 import React from 'react';
 
-export default function MovieCard (props) {
-  return;
+const MovieCard = props => {
+  if(!props){
+    return (
+    <div>
+      Loading movie information..
+    </div>);
+  };
+  const { title, director, metascore, stars} = props;
+  return (
+    <div className="save-wrapper">
+      <div className="movie-card">
+        <h2>{title}</h2>
+        <div className="movie-director">
+          Director: <em>{director}</em>
+        </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
+        <h3>Actors</h3>
+
+        {stars &&
+        stars.map(star => (
+          <div key={star} className="movie-star">
+            {star}
+          </div>
+        ))}
+      </div>
+      <div className="save-button">Save</div>
+    </div>
+
+
+  );
+
+  function BackButton({ children }) {
+    let history = useHistory()
+    return (
+      <div>
+        <h1>
+          Avenger: </h1>
+         <h1> {name} {nickname}</h1>
+        <button type="button" onClick={() => history.goBack()}>
+          Go Back
+        </button>
+      </div>
+    );
+  }
+    )
+  }
 }
